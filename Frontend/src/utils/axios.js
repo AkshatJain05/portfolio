76a967bace7597115a67f7864aa934a8d88f3c33
Akshat Jain <156729933+AkshatJain05@users.noreply.github.com>
2025-09-13 19:07:38 +0530

    Update axios.js

--- Frontend/src/utils/axios.js
+++ Frontend/src/utils/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
-  withCredentials: true, 
+const API = axios.create({
+ baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000",
+  withCredentials: true // <-- important for sending cookies
 });
 
-export default api;
+export default API;
