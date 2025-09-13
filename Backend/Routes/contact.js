a68faf312aca42516262e62eb290342275ae82aa
Akshat Jain <156729933+AkshatJain05@users.noreply.github.com>
2025-09-13 20:09:15 +0530

    Update contact.js

--- Backend/Routes/contact.js
+++ Backend/Routes/contact.js
@@ -21,7 +21,7 @@ router.post("/", async (req, res) => {
 });
 
 // Get all contacts - admin only
-router.get("/", auth, async (req, res) => {
+router.get("/", async (req, res) => {
   try {
     const contacts = await Contact.find();
     res.json(contacts);
